4092e4a88af337e9f8ea7d5d36ae9ec185fc6263
kevinguzman0 <guzmankevin90@gmail.com>
2023-01-11 14:48:16 -0500

    second commit

--- src/style-components/layout.style.component.ts
+++ src/style-components/layout.style.component.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const LayoutContainer = styled.div`
   @media only screen and (min-width: 700px) {
-    width: 90vw;
+    width: 70vw;
   }
 
   @media only screen and (max-width: 1176px) {
